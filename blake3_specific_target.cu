cfac337b46282271c9f9642023981a1d67225309
cery999 <cery.zhai@enflame-tech.com>
2023-03-16 08:25:28 +0800

    add blake3 specific target

---
+++ blake3_specific_target.cu
@@ -0,0 +1,9 @@
+#pragma once
+// includes, system
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <cooperative_groups.h>
+using namespace cooperative_groups;
